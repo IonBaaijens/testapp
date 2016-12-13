ba67b83c65e5f01ae5f2bc9212ff66434ba19673
IonBaaijens <ion.baaijens95@gmail.com>
2016-12-13 13:39:09 +0100

    Testing

--- script/les3a.js
+++ script/les3a.js
@@ -8,37 +8,23 @@ function loadScript() {
 // When device is ready
 function onDeviceReady() {
 //	For touch devices:
-	document.getElementById('btn1').addEventListener('touchstart', btn1Down,  false);
-	document.getElementById('btn1').addEventListener('touchend',   btn1Up, false);
-	document.getElementById('btn2').addEventListener('touchstart', btn2Down,  false);
-	document.getElementById('btn2').addEventListener('touchend',   btn2Up, false);
+	document.getElementById('menu').addEventListener('touchstart', menuDown,  false);
+	document.getElementById('menu').addEventListener('touchend',   menuUp, false);
 //	For mouse devices:
-	document.getElementById('btn1').addEventListener('mousedown', btn1Down,  false);
-	document.getElementById('btn1').addEventListener('mouseup',   btn1Up, false);
-	document.getElementById('btn2').addEventListener('mousedown', btn2Down,  false);
-	document.getElementById('btn2').addEventListener('mouseup',   btn2Up, false);
+	document.getElementById('menu').addEventListener('mousedown', menuDown,  false);
+	document.getElementById('menu').addEventListener('mouseup',   menuUp, false);
 }
 
 // Pressing the buttons only changes the visual apearence
-function btn1Down() {
-	document.getElementById('btn1').className='down';
+function menuDown() {
+	document.getElementById('menu').className='down';
 }
-function btn2Down() {
-	document.getElementById('btn2').className='down';
-}
+
 // This function is for button 1
-function btn1Up() {
+function menuUp() {
     // Reset the visual apearence to idle state
-	document.getElementById('btn1').className='up';
+	document.getElementById('menu').className='up';
     // Then do what button must do
 	document.getElementById('page1').className='showPage';
 	document.getElementById('page2').className='hidePage';
-}
-// This function is for button 2
-function btn2Up() {
-    // Reset the visual apearence to idle state
-	document.getElementById('btn2').className='up';
-    // Then do what button must do
-	document.getElementById('page1').className='hidePage';
-	document.getElementById('page2').className='showPage';
 }
